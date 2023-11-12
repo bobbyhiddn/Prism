52bbf7949fa4326db81e0eec10c10969ac2c5bcc
bobbyhiddn <mlmicahlongmire@gmail.com>
2023-11-12 15:29:26 -0700

    we're close

--- dynamic_labyrinth.py
+++ dynamic_labyrinth.py
@@ -19,20 +19,19 @@ def distance(square1, square2):
     return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 
 # Function to draw connection
-# Modified draw_connection function to create doors
 def draw_connection(x1, y1, x2, y2):
-    if x1 == x2:
-        for i in range(min(y1, y2) + 1, max(y1, y2)):
-            if grid[i][x1] == " ":
-                grid[i][x1] = "="
+    if x1 == x2:  # North-South corridor
+        for i in range(min(y1, y2), max(y1, y2) - 1):  # Stop before entering the room
+            if grid[i][x1] == " " or grid[i][x1] == "=":
+                grid[i][x1] = "="  # Corridor path
             elif grid[i][x1] == "#":
-                grid[i][x1] = "D"
-    elif y1 == y2:
-        for j in range(min(x1, x2) + 1, max(x1, x2)):
-            if grid[y1][j] == " ":
-                grid[y1][j] = "="
+                grid[i][x1] = "D"  # Door
+    elif y1 == y2:  # East-West corridor
+        for j in range(min(x1, x2), max(x1, x2) - 1):  # Stop before entering the room
+            if grid[y1][j] == " " or grid[y1][j] == "=":
+                grid[y1][j] = "="  # Corridor path
             elif grid[y1][j] == "#":
-                grid[y1][j] = "D"
+                grid[y1][j] = "D"  # Door
 
 # Grid size
 grid_size = 30
@@ -90,43 +89,49 @@ for (x1, y1), (x2, y2) in mst:
     mid_x1, mid_y1 = x1 + 3, y1 + 3
     mid_x2, mid_y2 = x2 + 3, y2 + 3
 
-    closest_x1 = min(range(x1 + 1, x1 + 5), key=lambda x: abs(x - mid_x2))
-    closest_y1 = min(range(y1 + 1, y1 + 5), key=lambda y: abs(y - mid_y2))
-    closest_x2 = min(range(x2 + 1, x2 + 5), key=lambda x: abs(x - mid_x1))
-    closest_y2 = min(range(y2 + 1, y2 + 5), key=lambda y: abs(y - mid_y1))
+    # Calculate the closest points excluding corners
+    closest_x1 = min(range(x1 + 2, x1 + 4), key=lambda x: abs(x - mid_x2))
+    closest_y1 = min(range(y1 + 2, y1 + 4), key=lambda y: abs(y - mid_y2))
+    closest_x2 = min(range(x2 + 2, x2 + 4), key=lambda x: abs(x - mid_x1))
+    closest_y2 = min(range(y2 + 2, y2 + 4), key=lambda y: abs(y - mid_y1))
 
+    # Draw the corridors using the non-corner points
     if closest_y1 < closest_y2:
-        draw_connection(closest_x1, closest_y1, closest_x1, closest_y2 - 1)
-        draw_connection(closest_x1, closest_y2, closest_x2, closest_y2)
+        draw_connection(closest_x1, closest_y1, closest_x1, closest_y2)
+        draw_connection(closest_x1, closest_y2 - 1, closest_x2, closest_y2 - 1)
     else:
-        draw_connection(closest_x1, closest_y1, closest_x1, closest_y2 + 1)
-        draw_connection(closest_x1, closest_y2, closest_x2, closest_y2)
+        draw_connection(closest_x1, closest_y1, closest_x1, closest_y2)
+        draw_connection(closest_x1, closest_y2 + 1, closest_x2, closest_y2 + 1)
 
 # Navigate the realm
-# Modified navigation loop to maintain doors and prevent moving into the void
+# Modified navigation loop to track the player's path and prevent exiting the map
 while True:
     for row in grid:
         print("".join(row))
     command = input("Command: ").lower()
-    x, y = player_position
-    def can_move_to(nx, ny):
-        return grid[ny][nx] in " =D" and (0 <= nx < grid_size and 0 <= ny < grid_size)
 
-    if command == 'n' and can_move_to(x, y - 1):
-        grid[y][x] = "D" if grid[y][x] == "0" and grid[y - 1][x] == "D" else " "
-        grid[y - 1][x] = '0'
+    x, y = player_position
+
+    def can_move_to(nx, ny):
+        # Check if the next position is within the grid boundaries
+        within_bounds = 0 <= nx < grid_size and 0 <= ny < grid_size
+        # Check if the next position is a valid path (corridor, door, or player trail)
+        is_valid_path = grid[ny][nx] in "=D. "
+        return within_bounds and is_valid_path
+
+    if command == 'n' and can_move_to(x, y - 1):  # North movement
         player_position = (x, y - 1)
-    elif command == 's' and can_move_to(x, y + 1):
-        grid[y][x] = "D" if grid[y][x] == "0" and grid[y + 1][x] == "D" else " "
-        grid[y + 1][x] = '0'
+    elif command == 's' and can_move_to(x, y + 1):  # South movement
         player_position = (x, y + 1)
-    elif command == 'w' and can_move_to(x - 1, y):
-        grid[y][x] = "D" if grid[y][x] == "0" and grid[y][x - 1] == "D" else " "
-        grid[y][x - 1] = '0'
-        player_position = (x - 1, y)
-    elif command == 'e' and can_move_to(x + 1, y):
-        grid[y][x] = "D" if grid[y][x] == "0" and grid[y][x + 1] == "D" else " "
-        grid[y][x + 1] = '0'
+    elif command == 'e' and can_move_to(x + 1, y):  # East movement
         player_position = (x + 1, y)
+    elif command == 'w' and can_move_to(x - 1, y):  # West movement
+        player_position = (x - 1, y)
     else:
         print("Invalid command or blocked path")
+        continue
+
+    # Update the grid with the player's new position
+    grid[y][x] = '.'  # Mark the trail of the player
+    new_x, new_y = player_position
+    grid[new_y][new_x] = '0'  # Mark new position
